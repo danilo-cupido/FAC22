439dcc7dba043e1ebddb378b0ac6db372dcba476
danilo-cupido <danilocupido@gmail.com>
2021-10-12 17:06:26 +0100

    form validation workshop part2

--- Week-3/form-validation/index.js
+++ Week-3/form-validation/index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+console.log('hello world');
+
+const form = document.querySelector('form');
+const inputs = Array.from(document.querySelectorAll('input'));
+
+form.setAttribute('novalidate', '');
+
+inputs.forEach((i) => {
+  i.setAttribute('aria-invalid', false);
+  i.addEventListener('invalid', () => {
+    i.setAttribute('aria-invalid', true);
+    console.log(i.validationMessage);
+    const errorId = i.id + 'Error';
+    const errorContainer = form.querySelector('#' + errorId);
+    errorContainer.textContent = i.validationMessage;
+  });
+});
+
+form.addEventListener('submit', (event) => {
+  const allInputsValid = form.checkValidity();
+  if (!allInputsValid) {
+    event.preventDefault();
+  }
+});

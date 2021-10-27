ba8e1cff0ceda6241cdea63b3d5f583344ea1581
danilo-cupido <danilocupido@gmail.com>
2021-10-27 11:08:34 +0100

    add react-refactor-classes workshop

---
+++ Week-5/react-refactor-classes/workshop/Keyboard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Keyboard() {
+  const [key, setKey] = React.useState('');
+  React.useEffect(() => {
+    const handleKeyDown = (event) => setKey(event.key);
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+  return <div>{key || 'Press any key'}</div>;
+}
+
+// class Keyboard extends React.Component {
+//   state = {
+//     key: "",
+//   };
+//   handleKeyDown = (event) => {
+//     this.setState({ key: event.key });
+//   };
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     // stops the event listener continuing to fire
+//     // when the component has left the page
+//     window.removeEventListener("keydown", this.handleKeyDown);
+//   }
+//   render() {
+//     return <div>{this.state.key || "Press any key"}</div>;
+//   }
+// }
+
+export default Keyboard;
